8366c07e32b43ccea26fb3c3912fdf236c03e5a2
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2025-03-10 20:14:51 +0100

    Created Project solution

---
+++ ProjGenspil/ProjGenspil/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjGenspil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
